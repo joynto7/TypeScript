6ce92eb89394cd83ce52dbe22cd5380ddb4f1112
joyntoghosh <joyntoghosh7@gmail.com>
2026-04-30 22:56:19 +0600

    Spread Operator in Objects

--- src/spreadRest.ts
+++ src/spreadRest.ts
@@ -9,3 +9,21 @@ friends.push(...collegeFriends)
 
 console.log(friends);
 
+// Objects
+
+const usr =
+ { 
+    name: "Sui", 
+    PhoneNo:"01759191945"
+}
+const otherInfo = 
+{
+     name: "Sui", 
+     PhoneNo:"01759191945"
+}
+
+const usrInfo = { ...usr, ...otherInfo}
+console.log(usrInfo);
+
+
+
