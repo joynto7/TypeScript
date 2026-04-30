99480d8fa25ae0c5224bd1ad45e5fded01da276f
joyntoghosh <joyntoghosh7@gmail.com>
2026-04-30 22:50:55 +0600

    Spread Operator with Array

---
+++ src/spreadRest.ts
@@ -0,0 +1,11 @@
+//Spread Operator in Array
+
+const friends = ["op","Suchi"];
+const schoolfriends = ["Mon","OVi"];
+const collegeFriends = ["mahin","Doha"];
+
+friends.push(...schoolfriends)
+friends.push(...collegeFriends)
+
+console.log(friends);
+
