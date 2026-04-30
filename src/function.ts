9343ffe2cac71775430f66f36c3ed0f9b0d407d6
joyntoghosh <joyntoghosh7@gmail.com>
2026-04-30 21:57:31 +0600

    Normal Functions and Array Functions

---
+++ src/function.ts
@@ -0,0 +1,14 @@
+//Functions
+
+//nomral Functions
+
+function add(num1:number, num2:number):number
+{
+  return num1 + num2;
+}
+
+//Array Functions
+const addArrow = (num1:number,num2:number):
+number => num1 + num2;
+
+addArrow(2,2);
