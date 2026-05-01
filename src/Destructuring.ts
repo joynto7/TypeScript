0ad0dbe4b363fa3669ddeb0c8a1e6e4f2d2e8ef5
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-01 23:16:21 +0600

    object  destructuring

--- src/Destructuring.ts
+++ src/Destructuring.ts
@@ -3,3 +3,15 @@ const numbers: number[] = [10, 20, 30];
 const [a, b, c] = numbers;
 
 console.log(a); // 10
+
+type User = {
+  name: string;
+  age: number;
+};
+// object desturicing 
+const user: User = {
+  name: "Alice",
+  age: 25
+};
+
+const { name, age } = user;
