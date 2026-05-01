2214e65410eb2f124cda668558243dd1a4371d4f
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-01 23:17:49 +0600

    array  destructuring

--- src/Destructuring.ts
+++ src/Destructuring.ts
@@ -15,3 +15,10 @@ const user: User = {
 };
 
 const { name, age } = user;
+const arr: number[] = [10, 20, 30];
+
+const [a, b, c] = arr;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30
