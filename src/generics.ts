4b93ac2948bc9c6bc11a679d500f9cd06f0597a4
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-02 21:14:23 +0600

    generics for objects

--- src/generics.ts
+++ src/generics.ts
@@ -25,3 +25,19 @@ type Coordinates<X,Y> = [X,Y]
 
 const coordinates1: Coordinates<number,number> = [20,30];
 const coordinates2: Coordinates<String,String> = ["20","30"]
+
+//generic for object
+
+const studentList: GenericArray<{name:string;age:number}> = [
+    { 
+    name: "joy",
+    age:23,
+    },
+    {
+        name:"pran",
+        age:34
+    }
+]
+console.log(studentList);
+
+
