ed9269eb30ab1cded9fa2478cc336c6d42516aa8
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-02 21:07:36 +0600

    generics Start

---
+++ src/generics.ts
@@ -0,0 +1,27 @@
+
+
+type GenericArray<T> = Array<T>;
+
+
+
+//const friends : string[]=["joy","Hoi","Mori",];
+
+const friends : GenericArray<string>=["joy","Hoi","Mori",];
+//const rollNumbers : number[]=[12,23,23,];
+
+const rollNumbers : GenericArray<number>=[12,23,23,];
+
+//const isEligiableList: boolean[] = [true,false,true];
+
+const isEligiableList: GenericArray<boolean> = [true,false,true];
+
+
+console.log(friends,rollNumbers,isEligiableList);
+
+
+//  using mulitple datatypes on genreics
+
+type Coordinates<X,Y> = [X,Y]
+
+const coordinates1: Coordinates<number,number> = [20,30];
+const coordinates2: Coordinates<String,String> = ["20","30"]
