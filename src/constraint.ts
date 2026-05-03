c04095cf06252642595d933248c6cd81dd66b993
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-03 11:18:59 +0600

    arrow function and object

--- src/constraint.ts
+++ src/constraint.ts
@@ -3,3 +3,34 @@ type Student = {
   name: string;
   age: number;
 };
+
+// arrow function
+
+const addStudentCourse = <T extends Student>(student: T) => {
+  return {
+    ...student,
+    course: "Cybersecurity",
+  };
+};
+
+//create objects
+
+const student1 = {
+  id: 1,
+  name: "Joy",
+  age: 22,
+};
+
+const student2 = {
+  id: 2,
+  name: "Rahim",
+  age: 21,
+  department: "CSE", // extra property
+};
+
+const student3 = {
+  id: 3,
+  name: "Karim",
+  age: 23,
+  isActive: true, // extra property
+};
